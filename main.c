3f9d04a2944600b1b8640e232d352381adb57afe
k9targex <dimamozheiko13@gmail.com>
2024-04-12 21:19:29 +0300

    feat: add .zip, .gzip

--- main.c
+++ main.c
@@ -30,23 +30,187 @@ void updateFileModificationTime(const char *filename, time_t newTime) {
     }
 }
 
+void remove_directory_recursive(const char *path) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+<<<<<<< HEAD
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            remove_directory_recursive(full_path); 
+        } else {
+            remove(full_path); 
+        }
+    }
+
+    closedir(dir);
+    rmdir(path); 
+}
+
+void process_directory(const char *path, const char *fileType, time_t *maxModificationTime) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            process_directory(full_path, fileType, maxModificationTime); 
+        } else if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
+            time_t modificationTime = getMaxFileModificationTime(full_path);
+            if (modificationTime > *maxModificationTime) {
+                *maxModificationTime = modificationTime;
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+void processFilesRecursively(const char *dirPath, const char *fileType, time_t maxModificationTime) {
+    DIR *dir = opendir(dirPath);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dirPath, entry->d_name);
+
+        if (entry->d_type == DT_DIR) { 
+            processFilesRecursively(full_path, fileType, maxModificationTime);
+        } else if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
+            updateFileModificationTime(full_path, maxModificationTime);
+        }
+    }
+
+    closedir(dir);
+}
+
+void processArchive(const char *archivePath, const char *fileType) {
+    char *dirName = strdup(dirname(strdup(archivePath)));
+    printf("dirNAME= %s\n",dirName);
+    char *tempDir = "temp";
+    char tempArchive[PATH_MAX];
+
+    remove_directory_recursive(tempDir); 
+
+=======
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            remove_directory_recursive(full_path); 
+        } else {
+            remove(full_path); 
+        }
+    }
+
+    closedir(dir);
+    rmdir(path); 
+}
+
+void process_directory(const char *path, const char *fileType, time_t *maxModificationTime) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            process_directory(full_path, fileType, maxModificationTime); 
+        } else if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
+            time_t modificationTime = getMaxFileModificationTime(full_path);
+            if (modificationTime > *maxModificationTime) {
+                *maxModificationTime = modificationTime;
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+void processFilesRecursively(const char *dirPath, const char *fileType, time_t maxModificationTime) {
+    DIR *dir = opendir(dirPath);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue; 
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dirPath, entry->d_name);
+
+        if (entry->d_type == DT_DIR) { 
+            processFilesRecursively(full_path, fileType, maxModificationTime);
+        } else if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
+            updateFileModificationTime(full_path, maxModificationTime);
+        }
+    }
+
+    closedir(dir);
+}
+
 void processArchive(const char *archivePath, const char *fileType) {
     char *dirName = strdup(dirname(strdup(archivePath)));
     char *tempDir = "temp";
     char tempArchive[PATH_MAX];
 
-    if (rmdir(tempDir) == -1 && errno != ENOENT) {
-        perror("rmdir");
-        return;
-    }
+    remove_directory_recursive(tempDir); 
 
-    // Create temporary directory
+>>>>>>> 8bed6c69b2d04af306b773d7369ac4e445bf8cad
     if (mkdir(tempDir, 0777) == -1) {
         perror("mkdir");
         return;
     }
 
-    // Extract the archive
     pid_t extractPid = fork();
     if (extractPid == -1) {
         perror("fork");
@@ -54,63 +218,73 @@ void processArchive(const char *archivePath, const char *fileType) {
     }
 
     if (extractPid == 0) {
-        // Child process
+<<<<<<< HEAD
+        // Выбор команды извлечения в зависимости от расширения имени файла
+        char *extension =  strrchr(archivePath, '.');
+        if (extension == NULL) {
+            printf("Cannot determine archive type.\n");
+            exit(1);
+        }
+        if (strcmp(extension, ".tar") == 0) {
+            execlp("tar", "tar", "-xvf", archivePath, "-C", tempDir, NULL);
+        } else if (strcmp(extension, ".zip") == 0) {
+            execlp("unzip", "unzip", archivePath, "-d", tempDir, NULL);
+        } else if (strcmp(extension, ".gz") == 0) {
+            execlp("gunzip", "gunzip", "-c", archivePath, ">", tempDir, NULL);
+        } else {
+            printf("Unsupported archive type.\n");
+            exit(1);
+        }
+=======
         execlp("tar", "tar", "-xvf", archivePath, "-C", tempDir, NULL);
+>>>>>>> 8bed6c69b2d04af306b773d7369ac4e445bf8cad
         perror("execlp");
         exit(1);
     } else {
-        // Parent process
         int extractStatus;
         waitpid(extractPid, &extractStatus, 0);
         if (WIFEXITED(extractStatus) && WEXITSTATUS(extractStatus) == 0) {
-            // Extraction successful
 
-            // Get the maximum modification time
-            DIR *dir;
-            struct dirent *entry;
-            dir = opendir(tempDir);
-            if (dir == NULL) {
-                perror("opendir");
-                return;
-            }
             time_t maxModificationTime = 0;
+            process_directory(tempDir, fileType, &maxModificationTime);
 
-            while ((entry = readdir(dir)) != NULL) {
-                if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
-                    char filePath[PATH_MAX];
-                    snprintf(filePath, sizeof(filePath), "%s/%s", tempDir, entry->d_name);
-
-                    time_t modificationTime = getMaxFileModificationTime(filePath);
-                    if (modificationTime > maxModificationTime) {
-                        maxModificationTime = modificationTime;
-                    }
-                }
-            }
-
-            closedir(dir);
-
-            // Update the modification time of all files in the temporary directory
-            dir = opendir(tempDir);
-            if (dir == NULL) {
-                perror("opendir");
-                return;
-            }
-
-            while ((entry = readdir(dir)) != NULL) {
-                if (entry->d_type == DT_REG && strstr(entry->d_name, fileType) != NULL) {
-                    char filePath[PATH_MAX];
-                    snprintf(filePath, sizeof(filePath), "%s/%s", tempDir, entry->d_name);
-
-                    updateFileModificationTime(filePath, maxModificationTime);
-                }
-            }
-
-            closedir(dir);
-
-            // Create a temporary archive with updated files
+<<<<<<< HEAD
+            processFilesRecursively(tempDir, fileType, maxModificationTime);
+   
             snprintf(tempArchive, sizeof(tempArchive), "%s_temp", archivePath);
             char command[PATH_MAX + 100];
-            snprintf(command, sizeof(command), "tar -cvf %s -C %s .", tempArchive, tempDir);
+            // Создание команды архивирования в зависимости от расширения имени файла
+            char *extension = strrchr(archivePath, '.');
+            if (extension == NULL) {
+                printf("Cannot determine archive type.\n");
+                return;
+            }
+            if (strcmp(extension, ".tar") == 0) {
+                snprintf(command, sizeof(command), "tar -cvf %s -C %s --exclude=%s .", tempArchive, tempDir, tempDir);
+            } else if (strcmp(extension, ".zip") == 0) {
+                char fullpathArchive[PATH_MAX];
+                realpath(tempArchive, fullpathArchive);
+                snprintf(command, sizeof(command), "cd temp/ && zip -r %s *", fullpathArchive);
+                printf("COMMAND = %s",command);
+                // printf("Path to tempDir = %s",fullTempDirPath);
+                // snprintf(command, sizeof(command), "cd %s && zip -r %s *", fullTempDirPath, tempArchive);
+
+
+            } else if (strcmp(extension, ".gz") == 0) {
+                snprintf(command, sizeof(command), "gzip -c %s > %s", tempDir, tempArchive);
+            } else {
+                printf("Unsupported archive type.\n");
+                return;
+            }
+=======
+            processFilesRecursively(tempDir,fileType,maxModificationTime);
+   
+            snprintf(tempArchive, sizeof(tempArchive), "%s_temp", archivePath);
+            char command[PATH_MAX + 100];
+            // snprintf(command, sizeof(command), "tar -cvf %s -C %s .", tempArchive, tempDir);
+            snprintf(command, sizeof(command), "tar -cvf %s -C %s --exclude=%s .", tempArchive, tempDir, tempDir);
+>>>>>>> 8bed6c69b2d04af306b773d7369ac4e445bf8cad
+
             int status = system(command);
             if (status == -1) {
                 perror("system");
@@ -120,33 +294,17 @@ void processArchive(const char *archivePath, const char *fileType) {
             if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
                 printf("Temporary archive created successfully!\n");
 
-                // Remove old archive
                 if (remove(archivePath) != 0) {
                     perror("remove");
                     return;
                 }
 
-                // Rename temporary archive to original archive name
                 if (rename(tempArchive, archivePath) != 0) {
                     perror("rename");
                     return;
                 }
 
-                // Remove temporary directory
-                dir = opendir(tempDir);
-                if (dir == NULL) {
-                    perror("opendir");
-                    return;
-                }
-
-                while ((entry = readdir(dir)) != NULL) {
-                    char filePath[PATH_MAX];
-                    snprintf(filePath, sizeof(filePath), "%s/%s", tempDir, entry->d_name);
-                    remove(filePath);
-                }
-
-                closedir(dir);
-                rmdir(tempDir);
+                remove_directory_recursive(tempDir);
             } else {
                 printf("Failed to create temporary archive.\n");
             }
@@ -157,12 +315,11 @@ void processArchive(const char *archivePath, const char *fileType) {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { 
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <archive_path> <file_type>\n", argv[0]);
         return 1;
     }
-
     const char *archivePath = argv[1];
     const char *fileType = argv[2];
     processArchive(archivePath, fileType);
